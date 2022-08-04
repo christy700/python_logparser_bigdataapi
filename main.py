7ad61dfb376f8de155c23c9cc61eeabc05f67059
christy jose <josechristy800@gmail.com>
2022-08-05 00:46:12 +0530

    my second commit

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ freq = input("Enter The Number Of Hit(Hit Per IP Count): ")
 
 if os.path.isfile(File_Name):
   access_lf = open(File_Name,"r")
-  
+
   if freq.isdigit():
     freq = int(freq)
 
   else:
     print("\nPlease Enter A Valid Hit Count(Value Must Be An Integer)")
     sys.exit(0)
-    
+
   api_key = secretmanager.api_fetch()
 
   if api_key == "noapi":
@@ -28,31 +28,30 @@ if os.path.isfile(File_Name):
   else:
     print("Successfully Retrived API from SecretManager")
     ipdict = {}
-    
+
     for logline in access_lf:
       logdict = logparser.parser(logline)
-      if logdict["host"] not in ipdict:     
+      if logdict["host"] not in ipdict:
         ipdict[logdict["host"]] = 1
-      
-      else:            
+
+      else:
         ipdict[logdict["host"]] = ipdict[logdict["host"]] + 1
 
   print("\n{:18} {:10} {:15} {:15}".format("IPAddress","HitCount","Theatlevel","Country"))
-  
+
   ip_sorted = sorted(ipdict.items(),key=sort_ip_count,reverse=True)
   for item in ip_sorted:
     ip,count = item
     if count >= freq:
       api_result = bigdata_api.bigdata_api(ip=ip,big_api_key=api_key)
-      
+
       if type(api_result) == list:
         print("{:15}{:10}      {:10}    {:5}".format(ip,count,api_result[1],api_result[0]))
       else:
-        print("Couldn't Retrive Details Of Country And Threat level As API Call Failed\n")
+        print("Couldn't Retrive Details Of Country And Threat level for ip:{}\n".format(ip))
         print("{:15}{:10}".format(ip,count))
-        
-  
-else:
-    
-  print("\nPlease enter a valid path(absolute path of access log). The given path is not valid!")
 
+
+else:
+
+  print("\nPlease enter a valid path(absolute path of access log). The given path is not valid!")
