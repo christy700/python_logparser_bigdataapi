f2b74533342434689b962856b31315f63cf83900
christy jose <josechristy800@gmail.com>
2022-08-05 09:53:06 +0530

    update secretmanager.py

--- secretmanager.py
+++ secretmanager.py
@@ -1,3 +1,6 @@
+import boto3
+import json
+
 def api_fetch():
 
   import boto3
@@ -8,7 +11,6 @@ def api_fetch():
     session = boto3.session.Session()
     client = session.client(service_name = 'secretsmanager', region_name = 'ap-south-1')
     response = client.get_secret_value(SecretId = 'prod/myapikey')
-  
     api_dict = json.loads(response["SecretString"])
     api_key = api_dict["bigdatakey"]
     return api_key
